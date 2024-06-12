0cc5464d3af9305fad2e0c9f79505485f87f01f2
chigoox <dikeemmanuel54@gmail.com>
2024-06-12 13:11:02 -0400

    fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
 import {
-  Inter, Poppins, Roboto
+  Roboto
 } from 'next/font/google'
 import './globals.css'
-import NavBar from '@/app/Componets/Header/NavBar.jsx'
 
 const inter = Roboto({ subsets: ['latin'], weight: ['400'] })
 
@@ -17,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
         {children}
       </body>
     </html>
